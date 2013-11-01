6ccc6f841db477ece38a68df87f93c541ab7da3b
knuttyse <knuttyse@gmail.com>
2013-11-02 00:15:33 +0100

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,76 @@
+from scitools.std import *
+import numpy as np
+oppgave= raw_input("hvilken deloppgave?")
+
+def extract2(filename):
+    infile = open(filename,'r')
+    i=0
+    counter=0
+    E_k=[]
+    E_pot=[]
+    L=[]
+
+    for line in infile:
+        words= line.split()
+        E_k.append(float(words[0]))
+        E_pot.append(float(words[1]))
+        L.append(float(words[2]))
+    return array(E_k), array(E_pot), array(L)
+
+def extract(filename):
+    infile = open(filename,'r')
+    i=0
+    counter=0
+
+    for line in infile:
+
+        words= line.split()
+
+        if counter==0:
+            k=int(words[0])
+            n=int(words[1])
+            N=int(words[2])
+            T=float(words[3])
+            matrix=zeros((n+1,k*N))
+            t=zeros((n+1))
+        if counter==1:
+            planets= words[:]
+
+        if counter>1:
+            for j in range(k*N):
+                print j, "  ",  i
+                t[i]=float(words[0])
+                matrix[i, j]=float(words[j+1])
+            i+=1
+        counter+=1
+    t=t[0:i]
+    print t
+    matrix=matrix[0:i,:]
+    
+    infile.close()
+    return k,n,N,T,t,planets, matrix
+
+
+readfile='data.txt'
+readfile2='data2.txt'
+k,n,N,T,t,planets,matrix = extract(readfile)
+savefile='../../doc/plot%sn%dT%g.png' % (oppgave, n,T)
+
+hold('on')
+for i in range(k): 
+    plot(matrix[:,i*N],matrix[:,i*N+1],markersize=0.2,legend=planets[i],title=('n=%d steps, T=%7.2f days' % (n,T) ),xlabel='x [AU]',ylabel='y [AU]',hardcopy= savefile)
+   
+if oppgave=="c":
+    theta=linspace(0,2*pi)
+    x=0.9976332324*cos(theta)
+    y=0.9976332324*sin(theta)
+    plot(x,y,'k')
+raw_input()
+hold('off')
+E_k,E_pot,L= extract2(readfile2)
+plot(t,E_k,legend='E_k',xlabel='time [days]',ylabel='energy [Msun*AU^2/day^2]',hardcopy='../../doc/ek%s%d%g.png'% (oppgave, n,T) )
+raw_input()
+plot(t,E_pot,legend='E_pot',xlabel='time [days]',ylabel='energy [Msun*AU^2/day^2]',hardcopy='../../doc/ep%s%d%g.png' % (oppgave, n,T))
+raw_input()
+plot(t,L,legend='L',xlabel='time [days]',ylabel='angular momentum [Msun*AU^2/day]',hardcopy='../../doc/L%s%d%g.png' % (oppgave, n,T) )
+raw_input()
